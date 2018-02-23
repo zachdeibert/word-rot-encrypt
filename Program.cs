4e8ca0528b6db5ad8f43162bf4ff846a78a02032
Zach Deibert <zachdeibert@gmail.com>
2018-02-22 22:36:22 -0600

    Created basic program

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Com.GitHub.ZachDeibert.WordRotEncrypt {
+    class Program {
+        static void PrintUsage() {
+            Console.Error.WriteLine("Usage: {0} <key> [text ...]", AppDomain.CurrentDomain.FriendlyName);
+            Console.Error.WriteLine("key     must be an integer");
+            Console.Error.WriteLine("text    is either the text to encrypt/decrypt, or the text is read over STDIN");
+            Environment.Exit(1);
+        }
+
+        static void Main(string[] args) {
+            if (args.Length < 1) {
+                Console.Error.WriteLine("Not enough arguments.");
+                PrintUsage();
+            } else {
+                int key;
+                if (int.TryParse(args[0], out key)) {
+                    // TODO Encrypt it
+                } else {
+                    Console.Error.WriteLine("Invalid key.");
+                    PrintUsage();
+                }
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
